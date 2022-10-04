b019f822c3fe1714713c7278f79acbb7231d191f
tobias-edwards <tobias.edwards@publicissapient.com>
2022-10-04 12:47:01 +0100

    render eleventy website

--- index.js
+++ index.js
@@ -4,10 +4,18 @@ const path = require("path");
 const app = express();
 const port = process.env.PORT || 1337;
 
-// sendFile will go here
-app.get("/", function (req, res) {
-  res.sendFile(path.join(__dirname, "/index.html"));
+app.get("/", function (_, res) {
+  console.log("Someone visited the home page!");
+  res.sendFile(path.join(__dirname, "/_site/index.html"));
 });
 
+app.get("/docs", function (_, res) {
+  console.log("Someone visited the docs page!");
+  res.sendFile(path.join(__dirname, "/_site/docs/index.html"));
+});
+
+app.use(express.static("_site/assets"));
+
 app.listen(port);
+
 console.log("Server started at http://localhost:" + port);
