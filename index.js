c508991426f8d7c08247537ca2a8e7bf199e1f7f
tobias-edwards <tobias.edwards@publicissapient.com>
2022-10-04 11:08:34 +0100

    render HTML

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
-const http = require('http');
+const express = require("express");
+const path = require("path");
 
-const server = http.createServer((request, response) => {
-    response.writeHead(200, {"Content-Type": "text/plain"});
-    response.end("Hello World!");
+const app = express();
+const port = process.env.PORT || 1337;
+
+// sendFile will go here
+app.get("/", function (req, res) {
+  res.sendFile(path.join(__dirname, "/index.html"));
 });
 
-const port = process.env.PORT || 1337;
-server.listen(port);
-
-console.log("Server running at http://localhost:%d", port);
+app.listen(port);
+console.log("Server started at http://localhost:" + port);
